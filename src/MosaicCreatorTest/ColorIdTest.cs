71860b2b782cb8b0072ee2617117d7622457694b
Maurus Calzaferri <maurus.calzaferri@ost.ch>
2023-12-15 20:16:48 +0100

    Add colorId

---
+++ src/MosaicCreatorTest/ColorIdTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MosaicCreator
+{
+    public class ColorIdTest
+    {
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(256)]
+        public void ShouldThrowIfArgumentIsOutOfRange(int id)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => ColorId.FromValue(id));
+        }
+    }
+}
