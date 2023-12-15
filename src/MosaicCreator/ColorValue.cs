0ac7f34684c3eb7f0480660b0bd19538bebaf0ce
Maurus Calzaferri <maurus.calzaferri@ost.ch>
2023-12-15 20:51:27 +0100

    Rename and add color value of color

---
+++ src/MosaicCreator/ColorValue.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MosaicCreator
+{
+    internal struct ColorValue
+    {
+        public static readonly ColorValue Min = new ColorValue(0);
+        public static readonly ColorValue Max = new ColorValue(35);
+
+        private ColorValue(int value)
+        {
+            AsInt = value;
+        }
+
+
+        public static ColorValue FromValue(int value)
+        {
+            if (value < Min.AsInt | value > Max.AsInt)
+            {
+                throw new ArgumentOutOfRangeException($"Id {value} is out of range of allowed values [{Min.AsInt},{Max.AsInt}]");
+            }
+
+            return new ColorValue(value);
+        }
+
+        public static ColorValue Of(Color color)
+        {
+            var hue = color.GetHue();
+            var value = (int)((Max.AsInt + 1) * (hue / 360));
+            if (value == Max.AsInt + 1)
+            {
+                value = 0;
+            }
+            return new ColorValue(value);
+        }
+
+        public int AsInt { get; }
+    }
+}
---
+++ src/MosaicCreatorTest/ColorValueTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MosaicCreator
+{
+    public class ColorValueTest
+    {
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(10000)]
+        public void ShouldThrowIfArgumentIsOutOfRange(int id)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => ColorValue.FromValue(id));
+        }
+
+        [Fact]
+        public void ColorValueShouldCorrespondToHue()
+        {
+            var testData = new[] { (Color.Red, 0), (Color.Green, 120), (Color.Blue, 240) };
+            foreach (var item in testData)
+            {
+                var colorValue = ColorValue.Of(item.Item1);
+                Assert.Equal(item.Item2, (colorValue.AsInt * 360) / (ColorValue.Max.AsInt + 1));
+            }
+        }
+    }
+}
