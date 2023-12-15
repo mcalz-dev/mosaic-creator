fcd94d1b8457bc5d19f5b54cedf274b0f65ce96e
Maurus Calzaferri <maurus.calzaferri@ost.ch>
2023-12-15 02:04:15 +0100

    Add logic to load tile images into the working directory

--- src/MosaicCreator/Program.cs
+++ src/MosaicCreator/Program.cs
@@ -1,9 +1,51 @@
-﻿namespace MosaicCreator
+﻿using Microsoft.Extensions.Configuration;
+using System.Drawing;
+using System.Runtime.Intrinsics.Arm;
+
+namespace MosaicCreator
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            var configuration = (IConfiguration)new ConfigurationBuilder()
+                .SetBasePath(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "MosaicCreator"))
+                .AddJsonFile("mosaicCreatorConfig.json")
+                .Build();
+            var inputImagePath = configuration["inputImagePath"]!;
+            var outputImagePath = configuration["outputImagePath"]!;
+            var mosaicTilesDirectory = configuration["mosaicTilesDirectory"]!;
+            var workingDirectory = configuration["workingDirectory"]!;
+            var maxTileSize = new Size(64, 64);
+
+
+            foreach (var imageFile in EnumerateImageFiles(mosaicTilesDirectory))
+            {
+                var fingerprint = CalculateFingerprintOfFile(imageFile);
+                var processedFilePath = Path.Combine(workingDirectory, fingerprint + ".png");
+                if (File.Exists(processedFilePath))
+                {
+                    continue;
+                }
+
+                using var image = Image.FromFile(imageFile);
+                using var processedImage = image.Scale(maxTileSize);
+                processedImage.Save(processedFilePath);
+            }
+        }
+
+        static IEnumerable<string> EnumerateImageFiles(string directory)
+        {
+            var extensions = new string[] { "jpg", "jpeg", "png", "gif", "tiff", "bmp", "svg" };
+            return Directory.EnumerateFiles(directory, "*.*", SearchOption.AllDirectories).Where(file => extensions.Any(extension => file.ToLowerInvariant().EndsWith(extension)));
+        }
+
+        static string CalculateFingerprintOfFile(string filePath)
+        {
+            using var md5 = System.Security.Cryptography.MD5.Create();
+            using var stream = File.OpenRead(filePath);
+            byte[] hash = md5.ComputeHash(stream);
+            return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
         }
     }
 }
