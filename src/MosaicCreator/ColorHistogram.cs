ff6a4ffe76f82d3c54f633d27352ea2b02b964dc
Maurus Calzaferri <maurus.calzaferri@ost.ch>
2023-12-15 21:04:02 +0100

    Add color histogram

---
+++ src/MosaicCreator/ColorHistogram.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MosaicCreator
+{
+    internal class ColorHistogram
+    {
+        private int[] _data;
+        private long _count;
+
+        public ColorHistogram()
+        {
+            _data = new int[ColorValue.Max.AsInt];
+        }
+
+        public void IncrementColorCount(Color color)
+        {
+            _data[ColorValue.Of(color).AsInt]++;
+            _count++;
+        }
+
+        public int GetColorCount(Color color)
+        {
+            return _data[ColorValue.Of(color).AsInt];
+        }
+
+        public double GetColorPercentage(Color color)
+        {
+            return GetColorCount(color) / (double)_count;
+        }
+    }
+}
---
+++ src/MosaicCreatorTest/ColorHistogramTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MosaicCreator
+{
+    public class ColorHistogramTest
+    {
+        [Fact]
+        public void ShouldReturnCorrectColorCount()
+        {
+            var colorHistogram = new ColorHistogram();
+            colorHistogram.IncrementColorCount(Color.Red);
+            colorHistogram.IncrementColorCount(Color.Blue);
+            colorHistogram.IncrementColorCount(Color.Red);
+
+            Assert.Equal(2, colorHistogram.GetColorCount(Color.Red));
+            Assert.Equal(1, colorHistogram.GetColorCount(Color.Blue));
+        }
+
+        [Fact]
+        public void ShouldReturnCorrectColorPercentage()
+        {
+            var colorHistogramm = new ColorHistogram();
+            colorHistogramm.IncrementColorCount(Color.Red);
+            colorHistogramm.IncrementColorCount(Color.Blue);
+            colorHistogramm.IncrementColorCount(Color.Red);
+            colorHistogramm.IncrementColorCount(Color.Red);
+
+            Assert.Equal(0.75, colorHistogramm.GetColorPercentage(Color.Red), 0.001);
+            Assert.Equal(0.25, colorHistogramm.GetColorPercentage(Color.Blue), 0.001);
+        }
+    }
+}
