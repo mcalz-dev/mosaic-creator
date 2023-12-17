4e13f12e955f946eaca2dc88cb563f74e98d899d
Maurus Calzaferri <maurus.calzaferri@ost.ch>
2023-12-17 21:41:12 +0100

    Add to bitmask function for faster processing of histogram

---
+++ src/MosaicCreator/EnumerableExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MosaicCreator
+{
+    internal static class EnumerableExtensions
+    {
+        public static uint ToBitmask<T>(this IEnumerable<T> values, Predicate<T> threshold)
+            where T : struct, IConvertible
+        {
+            return ToBitmask(values, threshold, (a, b) => (T)Convert.ChangeType(Convert.ToDouble(a) + Convert.ToDouble(b), typeof(T)));
+        }
+
+        public static uint ToBitmask<T>(this IEnumerable<T> values, Predicate<T> threshold, Func<T,T,T> accumulatorFunction)
+            where T : struct
+        {
+            const int NumberOfBits = 32;
+            var size = values.Count();
+            var valuesArray = values.ToArray();
+            var bitmaskData = new T[NumberOfBits];
+            for (var i = 0; i < NumberOfBits; i++)
+            {
+                bitmaskData[i] = default;
+            }
+
+            for (var i = 0; i < size; i++)
+            {
+                var index = (NumberOfBits * i) / size;
+                bitmaskData[index] = accumulatorFunction(bitmaskData[index], valuesArray[i]);
+            }
+
+            uint result = 0;
+            for (var i = 0; i < NumberOfBits; i++)
+            {
+                result <<= 1;
+                if (threshold.Invoke(bitmaskData[i]))
+                {
+                    result += 1;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/MosaicCreatorTest/EnumerableExtensionsToBitmaskTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MosaicCreator
+{
+    public class EnumerableExtensionsToBitmaskTest
+    {
+        [Theory]
+        [InlineData(new[] {1, -1, 1, -1}, 0b_1000_0000_0000_0000_1000_0000_0000_0000)]
+        [InlineData(new[] {1, 1, 1, 1, 1, 1, 1, 1 }, 0b_1000_1000_1000_1000_1000_1000_1000_1000)]
+        public void ToBitmaskShouldWorkForSmallArrays(int[] ints, uint expectedBitmask)
+        {
+            var actualBitmask = ints.ToBitmask(x => x > 0);
+
+            Assert.Equal(expectedBitmask, actualBitmask);
+        }
+
+        [Fact]
+        public void ToBitmaskShouldWorkForLargeArrays()
+        {
+            var expectedBitmask = 0b_1000_1000_1000_1000_1000_1000_1000_1000;
+            var data = Enumerable.Repeat(0, 64).ToArray();
+            for (var i = 0; i < data.Length; i += 64 / 8)
+            {
+                data[i] = 1;
+            }
+
+            var actualBitmask = data.ToBitmask(x => x > 0);
+
+            Assert.Equal(expectedBitmask, actualBitmask);
+        }
+    }
+}
