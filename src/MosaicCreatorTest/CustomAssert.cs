39f08bce1dda2cc6bc303e01ac6a07eb39ecc3a5
Maurus Calzaferri <maurus.calzaferri@ost.ch>
2023-12-20 16:57:22 +0100

    Add custom assert functions for tests

---
+++ src/MosaicCreatorTest/CustomAssert.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MosaicCreator
+{
+    public static class CustomAssert
+    {
+        public static void Equal(Color expected, Color actual)
+        {
+            Assert.Equal(expected.R, actual.R);
+            Assert.Equal(expected.G, actual.G);
+            Assert.Equal(expected.B, actual.B);
+            Assert.Equal(expected.A, actual.A);
+        }
+    }
+}
